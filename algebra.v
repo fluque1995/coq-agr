0a49139af48b7a6780e0cf43695650f454e860b6
Nacho Mas <nacho.cronos2@gmail.com>
2018-05-18 13:21:59 +0200

    Adds center subgalgebra theorem

--- algebra.v
+++ algebra.v
@@ -1,9 +1,7 @@
 Set Automatic Coercions Import.
 Set Implicit Arguments.
 Unset Strict Implicit.
-Add LoadPath "/home/ritaso/.opam/system/bin".
 From Algebra Require Import Algebra.
-From Algebra Require Import Field_cat.
 From Coq.Setoids Require Import Setoid.
 From Algebra Require Import Algebra_facts.
 
@@ -14,24 +12,64 @@ Variable x : A.
 Definition is_center_element (x : A) : Type :=
   forall y : A, Equal (algebra_mult x y) (algebra_mult y x).
 
-Theorem centre_A_closed : (forall x y : A, is_center_element x -> is_center_element y -> is_center_element (algebra_mult x y)).
+
+Theorem unit_is_center : is_center_element (ring_algebra_unit A).
+Proof.
+  intros x.
+  auto with algebra.
+  refine (@Trans _ (algebra_mult (ring_algebra_unit A) x) x (algebra_mult x (ring_algebra_unit A)) _ _).
+  apply ring_algebra_unit_l.
+  apply Sym.
+  apply ring_algebra_unit_r.
+Qed.
+
+
+Theorem center_A_closed : (forall x y : A, is_center_element x -> is_center_element y -> is_center_element (algebra_mult x y)).
 Proof.
   intros x y.
   intros x_center y_center.
   intros z.
-  apply RING_ALGEBRA_assoc.
-  refine (Trans _ _).
-    exact ring_algebra_assoc.
-  (*
-  replace (algebra_mult (algebra_mult x y) z) with (algebra_mult x (algebra_mult y z)).
-  replace (algebra_mult x (algebra_mult y z)) with (algebra_mult x (algebra_mult z y)).
-  replace (algebra_mult x (algebra_mult z y)) with (algebra_mult (algebra_mult x z) y).
-  replace (algebra_mult (algebra_mult x z) y) with (algebra_mult (algebra_mult z x) y).
-  replace (algebra_mult (algebra_mult z x) y) with (algebra_mult z (algebra_mult x y)).
-  apply Refl.
+  refine (@Trans _ _ (algebra_mult x (algebra_mult y z)) _ _ _).
+  exact (ring_algebra_assoc A x y z).
+  refine (@Trans _ (algebra_mult x (algebra_mult y z)) (algebra_mult x (algebra_mult z y)) (algebra_mult z (algebra_mult x y)) _ _).
+  refine (ALGEBRA_comp _ _).
+  exact (@Refl _ x).
+  apply y_center.
+  refine (@Trans _ (algebra_mult x (algebra_mult z y)) (algebra_mult (algebra_mult x z) y) (algebra_mult z (algebra_mult x y)) _ _).
+  apply Sym.
+  exact (ring_algebra_assoc A x z y).
+  refine (@Trans _ (algebra_mult (algebra_mult x z) y) (algebra_mult (algebra_mult z x) y) (algebra_mult z (algebra_mult x y)) _ _).
+  refine (ALGEBRA_comp _ _).
+  apply x_center.
+  exact (Refl y).
+  refine (@Trans _ (algebra_mult (algebra_mult z x) y) (algebra_mult z (algebra_mult x y)) (algebra_mult z (algebra_mult x y)) _ _).
+  exact (ring_algebra_assoc A z x y).
+  refine (ALGEBRA_comp _ _).
+  exact (Refl z).
+  refine (ALGEBRA_comp _ _).
+  exact (Refl x).
+  exact (Refl y).
+Qed.
+
+
+Theorem center_is_comm : (forall x y : A, is_center_element x -> is_center_element y -> Equal (algebra_mult x y) (algebra_mult y x)).
+Proof.
+  intros x y.
+  intros x_center y_center.
+  apply x_center.
+Qed.
+
+Theorem center_is_submodule : (forall x y : A, is_center_element x -> is_center_element y -> is_center_element (sgroup_law A x y)).
+Proof.
+  intros x y.
+  intros x_center y_center.
+  intros z.
+  refine (@Trans _ _ (sgroup_law A (algebra_mult x z) (algebra_mult y z)) _ _ _).
+  exact (ALGEBRA_lin_left x y z).
+  refine (@Trans _ _ (sgroup_law A (algebra_mult z x) (algebra_mult z y)) _ _ _).
+  auto with algebra.
+  apply Sym.
+  exact (ALGEBRA_lin_right z x y).
+Qed.
+
   
-  unfold algebra_mult in |- *.
-  refine (@Leibnitz_set_prop _ x_is_leibnitz y_is_leibnitz _).
-    apply Sym.
-    refine (@RING_ALGEBRA_assoc _ _ _ _ _).
-   *)
